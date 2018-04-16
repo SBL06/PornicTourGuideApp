73ec3bb1a8cf63942e3dbce388be474257de561f
SBL06 <sim.bloch4@gmail.com>
2018-04-16 14:55:41 -0400

    Populated database

--- app/src/main/java/androidbasicsnanodegree/sbl/pornictourguideapp/MainActivity.java
+++ app/src/main/java/androidbasicsnanodegree/sbl/pornictourguideapp/MainActivity.java
@@ -50,6 +50,81 @@ public class MainActivity extends AppCompatActivity implements LocationFragment.
                 R.integer.castleCategory, R.drawable.castleImage, R.string.castleDescription,
                 R.string.castleInformations, R.raw.castle);
 
+        //faiencerie
+        addLocation(dataList, R.string.faiencerieName, R.string.faiencerieOpeningDays,
+                R.integer.faiencerieCategory, R.drawable.faiencerieImage, R.string.faiencerieDescription,
+                R.string.faiencerieInformations, R.raw.faiencerie);
+
+        //piverre
+        addLocation(dataList, R.string.piverreName, R.string.piverreOpeningDays,
+                R.integer.piverreCategory, R.drawable.piverreImage, R.string.piverreDescription,
+                R.string.piverreInformations, R.raw.piverre);
+
+        //aquacenter
+        addLocation(dataList, R.string.aquacenterName, R.string.aquacenterOpeningDays,
+                R.integer.aquacenterCategory, R.drawable.aquacenterImage, R.string.aquacenterDescription,
+                R.string.aquacenterInformations, R.raw.aquacenter);
+
+        //aventure
+        addLocation(dataList, R.string.aventureName, R.string.aventureOpeningDays,
+                R.integer.aventureCategory, R.drawable.aventureImage, R.string.aventureDescription,
+                R.string.aventureInformations, R.raw.aventure);
+
+        //valstmartin
+        addLocation(dataList, R.string.valstmartinName, R.string.valstmartinOpeningDays,
+                R.integer.valstmartinCategory, R.drawable.valstmartinImage, R.string.valstmartinDescription,
+                R.string.valstmartinInformations, R.raw.valstmartin);
+
+        //villanoe
+        addLocation(dataList, R.string.villanoeName, R.string.villanoeOpeningDays,
+                R.integer.villanoeCategory, R.drawable.villanoeImage, R.string.villanoeDescription,
+                R.string.villanoeInformations, R.raw.villanoe);
+
+        //casinorestaurant
+        addLocation(dataList, R.string.casinorestaurantName, R.string.casinorestaurantOpeningDays,
+                R.integer.casinorestaurantCategory, R.drawable.casinorestaurantImage, R.string.casinorestaurantDescription,
+                R.string.casinorestaurantInformations, R.raw.casinorestaurant);
+
+        //casahernesto
+        addLocation(dataList, R.string.casahernestoName, R.string.casahernestoOpeningDays,
+                R.integer.casahernestoCategory, R.drawable.casahernestoImage, R.string.casahernestoDescription,
+                R.string.casahernestoInformations, R.raw.casahernesto);
+
+        //crescendo
+        addLocation(dataList, R.string.crescendoName, R.string.crescendoOpeningDays,
+                R.integer.crescendoCategory, R.drawable.crescendoImage, R.string.crescendoDescription,
+                R.string.crescendoInformations, R.raw.crescendo);
+
+        //beausoleil
+        addLocation(dataList, R.string.beausoleilName, R.string.beausoleilOpeningDays,
+                R.integer.beausoleilCategory, R.drawable.beausoleilImage, R.string.beausoleilDescription,
+                R.string.beausoleilInformations, R.raw.beausoleil);
+
+        //aliance
+        addLocation(dataList, R.string.alianceName, R.string.alianceOpeningDays,
+                R.integer.alianceCategory, R.drawable.alianceImage, R.string.alianceDescription,
+                R.string.alianceInformations, R.raw.aliance);
+
+        //voletsbleus
+        addLocation(dataList, R.string.voletsbleusName, R.string.voletsbleusOpeningDays,
+                R.integer.voletsbleusCategory, R.drawable.voletsbleusImage, R.string.voletsbleusDescription,
+                R.string.voletsbleusInformations, R.raw.voletsbleus);
+
+        //lilirose
+        addLocation(dataList, R.string.liliroseName, R.string.liliroseOpeningDays,
+                R.integer.liliroseCategory, R.drawable.liliroseImage, R.string.liliroseDescription,
+                R.string.liliroseInformations, R.raw.lilirose);
+
+        //belleilloise
+        addLocation(dataList, R.string.belleilloiseName, R.string.belleilloiseOpeningDays,
+                R.integer.belleilloiseCategory, R.drawable.belleilloiseImage, R.string.belleilloiseDescription,
+                R.string.belleilloiseInformations, R.raw.belleilloise);
+
+        //comptoir
+        addLocation(dataList, R.string.comptoirName, R.string.comptoirOpeningDays,
+                R.integer.comptoirCategory, R.drawable.comptoirImage, R.string.comptoirDescription,
+                R.string.comptoirInformations, R.raw.comptoir);
+
         return dataList;
     }
 
